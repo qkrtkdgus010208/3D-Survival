805da76914da35f9c4a2886bc8a37c35d2679087
qkrtkdgus010208 <psh010208@naver.com>
2025-11-06 16:29:32 +0900

    1-3. 3D 기초 3강 플레이어 만들기 - 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+// Player와 관련된 기능을 모아두는 곳.
+// 이곳을 통해 기능에 각각 접근. (ex.CharacterManager.Instance.Player.controller)
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void Awake()
+    {
+        // 싱글톤매니저에 Player를 참조할 수 있게 데이터를 넘긴다.
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+    }
+}
