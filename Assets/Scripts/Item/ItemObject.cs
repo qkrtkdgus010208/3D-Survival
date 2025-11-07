df295fce64a258bc242168074ecadbc42f94a9b6
qkrtkdgus010208 <psh010208@naver.com>
2025-11-07 19:15:35 +0900

    1-10. 3D 기초 10강 아이템과 상호작용

---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+// 인터랙션 가능한 객체에 상속할 인터페이스
+public interface IInteractable
+{
+    public string GetInteractPrompt();  // UI에 표시할 정보
+    public void OnInteract();           // 인터랙션 호출
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        //Player 스크립트 먼저 수정
+        //Player 스크립트에 상호작용 아이템 data 넘기기.
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,67 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f;    // 상호작용 오브젝트 체크 시간
+    private float lastCheckTime;       // 마지막 상호작용 체크 시간
+    public float maxCheckDistance;     // 최대 체크 거리
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;  // 현재 상호작용 게임오브젝트
+    private IInteractable curInteractable;    // 현재 상호작용 인터페이스
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            // 2강 Ray 관련 로직 복습하기
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+            if (Physics.Raycast(ray, out RaycastHit hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetInteractPrompt();
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
