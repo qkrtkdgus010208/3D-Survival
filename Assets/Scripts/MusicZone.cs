bd3703c6f0ea0c4d384c76cdffce8e081b583d54
qkrtkdgus010208 <psh010208@naver.com>
2025-11-10 16:44:00 +0900

    1-17. 3D 기초 17강 발걸음 소리와 뮤직 존 생성

---
+++ Assets/Scripts/MusicZone.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class MusicZone : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float fadeTime;          // 변화 시간 주기
+    public float maxVolume;         // 최대 볼륨
+    private float targetVolume;     // 타겟 볼륨 (0과 maxVolume을 오갈 예정)
+
+    private void Start()
+    {
+        targetVolume = 0.0f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = targetVolume;
+        audioSource.Play();
+
+    }
+
+    private void Update()
+    {
+        // Approximately는 근사값 체크 → 이유를 꼭 학습하기
+        // (0.1f, 1*0.1f, 1f/10을 == 으로 비교하면 true가 나올까?) 
+        if (!Mathf.Approximately(audioSource.volume, targetVolume))
+        {
+            // 유니티 공식문서에서 각 매개변수 역할 확인
+            // (링크 여기)
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, targetVolume, (maxVolume / fadeTime) * Time.deltaTime);
+        }
+    }
+
+    // 뮤직존에 플레이어가 들어오면 타겟 볼륨을 maxVolume으로
+    // (점점 커지게)
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("진입");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("진입1");
+            targetVolume = maxVolume;
+        }
+    }
+
+    // 뮤직존에 플레이어가 들어오면 타겟 볼륨을 0으로
+    // (점점 작아지게)
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("탈출");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("탈출1");
+            targetVolume = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/FootSteps.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    public AudioClip[] footstepClips;     // 랜덤 재생시켜줄 오디오클립들
+    private AudioSource audioSource;      // 오디오 클립을 재생시킬 오디오소스
+    private Rigidbody _rigidbody;
+    public float footstepThreshold;       // 플레이 가능한 움직임 크기(조건)
+    public float footstepRate;            // 오디오 플레이 주기
+    private float footStepTime;           // 마지막 플레이 시간
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        // velocity(변화량)가 0보다 작으면 추락중
+        // velcotiy가 0보다(대략 0.1보다)크면 올라가는 중
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.1f) // 땅에 붙어 있을 때
+        {
+            if (_rigidbody.velocity.magnitude > footstepThreshold) // 움직일 때
+            {
+                if (Time.time - footStepTime > footstepRate) // 상호작용 로직 복습
+                {
+                    footStepTime = Time.time;
+                    audioSource.PlayOneShot(footstepClips[Random.Range(0, footstepClips.Length)]);
+                }
+            }
+        }
+    }
+}
