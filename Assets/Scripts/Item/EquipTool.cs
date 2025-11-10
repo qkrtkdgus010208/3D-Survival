3954de0c16cf2003be410b0fe3b992ea8f996306
qkrtkdgus010208 <psh010208@naver.com>
2025-11-10 11:16:03 +0900

    1-13. 3D 기초 13강 자원 채취

--- Assets/Scripts/Item/EquipTool.cs
+++ Assets/Scripts/Item/EquipTool.cs
@@ -36,4 +36,18 @@ public class EquipTool : Equip
     {
         attacking = false;
     }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Item/Resource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public ItemData itemToGive;   // 드랍 할 아이템 정보(SO)
+    public int quantityPerHit = 1;  // 한 번에 드랍할 갯수
+    public int capacity;          // 드랍 가능 횟수
+
+    public void Gather(Vector3 hitPoint, Vector3 hitNormal)
+    {
+        for (int i = 0; i < quantityPerHit; i++)
+        {
+            if (capacity <= 0) break;
+
+            capacity -= 1;
+            Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
+        }
+
+        if (capacity <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
