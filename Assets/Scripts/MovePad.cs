82051df93d659127935d2212853a54418ec7db73
qkrtkdgus010208 <psh010208@naver.com>
2025-11-11 13:40:48 +0900

    feat: 움직이는 플랫폼 구현

---
+++ Assets/Scripts/MovePad.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class MovePad : MonoBehaviour
+{
+    public Vector3 endOffset;
+    public float moveDuration = 5f;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Tween moveTween;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        endPosition = startPosition + endOffset;
+
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        if (moveDuration <= 0) return;
+
+        moveTween = transform.DOMove(endPosition, moveDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetLink(gameObject)
+            .SetUpdate(UpdateType.Fixed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
